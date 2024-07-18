7dbb5a882c3443d3ae537f5cb8b8bd9f34cf42b2
The-Best-Codes <best-codes@email.com>
2024-07-18 15:30:54 -0500

    update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -7,19 +7,8 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <h1 className="text-3xl font-bold">BestCodes</h1>
-      <motion.div
-        initial={{ opacity: 0 }}
-        animate={{ opacity: 1 }}
-        transition={{ duration: 0.5 }}
-      >
-        <Link href="/about">
-          <Image
-            src="https://avatars.githubusercontent.com/u/10091844?v=4"
-            width={200}
-            height={200}
-            alt="BestCodes"
-          />
-        </Link>
+      <motion.div initial={{ opacity: 0 }} whileInView={{ opacity: 1 }}>
+        <span className="text-3xl font-bold">Hello</span>
       </motion.div>
     </main>
   );
