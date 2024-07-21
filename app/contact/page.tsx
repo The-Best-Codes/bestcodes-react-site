c418fce6cbc6a78984b4acda615172fbca2f8aed
The-Best-Codes <best-codes@email.com>
2024-07-21 16:38:54 -0500

    build(deps): update header.tsx, package-lock.json and package.json

---
+++ app/contact/page.tsx
@@ -0,0 +1,192 @@
+"use client";
+import React, { useEffect, useState, useRef } from "react";
+import Link from "next/link";
+import Image from "next/image";
+import Header from "@/components/website/header";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import axios from "axios";
+import { Turnstile } from "@marsidev/react-turnstile";
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { Textarea } from "@/components/ui/textarea";
+import {
+  Card,
+  CardHeader,
+  CardFooter,
+  CardTitle,
+  CardDescription,
+  CardContent,
+} from "@/components/ui/card";
+import { Check } from "lucide-react";
+
+const formSchema = z.object({
+  name: z.string().min(2, {
+    message: "Name must be at least 2 characters.",
+  }),
+  email: z.string().email({
+    message: "Invalid email address.",
+  }),
+  message: z.string().min(10, {
+    message: "Message must be at least 10 characters.",
+  }),
+});
+
+export default function Contact() {
+  const [isSubmitting, setIsSubmitting] = useState(false);
+  const [isSuccess, setIsSuccess] = useState(false);
+  const [isError, setIsError] = useState(false);
+  const turnstileRef = useRef<any>(null);
+
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      name: "",
+      email: "",
+      message: "",
+    },
+  });
+
+  async function onSubmit(values: z.infer<typeof formSchema>) {
+    setIsSubmitting(true);
+    setIsError(false);
+
+    if (!turnstileRef.current) {
+      setIsSubmitting(false);
+      setIsError(true);
+      return;
+    }
+
+    const cloudflareToken = turnstileRef.current.getResponse();
+
+    try {
+      const response = await axios.post("/api/contact/create", {
+        ...values,
+        cloudflareToken,
+      });
+      if (response.status === 200) {
+        setIsSuccess(true);
+        form.reset();
+        turnstileRef.current.reset();
+      } else {
+        setIsError(true);
+      }
+    } catch (error) {
+      setIsError(true);
+    }
+
+    setIsSubmitting(false);
+  }
+
+  return (
+    <main className="flex min-h-screen scroll-smooth max-w-screen w-full flex-col items-center dark:bg-slate-900">
+      <Header />
+
+      <section className="flex flex-col items-center justify-center w-full flex-1 px-20 text-center">
+        <div className="mb-8">
+          <h1 className="text-5xl font-bold text-center dark:text-white">
+            Contact Me
+          </h1>
+          <p className="mt-3 text-lg text-center dark:text-white">
+            Fill out the simple form below to send me a message
+          </p>
+        </div>
+        <Card className="dark:border-none w-full max-w-3xl mx-auto">
+          <CardContent className="dark:bg-slate-700 dark:text-white rounded-lg flex flex-col p-8">
+            {isSuccess ? (
+              <div className="max-w-lg w-lg mx-auto">
+                <div className="flex items-center justify-center mb-4">
+                  <Check className="w-8 h-8 text-green-500" />
+                </div>
+                <p className="text-xl text-center dark:text-white">
+                  Message sent successfully!
+                </p>
+              </div>
+            ) : (
+              <Form {...form}>
+                <form
+                  onSubmit={form.handleSubmit(onSubmit)}
+                  className="space-y-8 w-full"
+                >
+                  <FormField
+                    control={form.control}
+                    name="name"
+                    render={({ field }) => (
+                      <FormItem className="flex flex-col justify-left">
+                        <FormLabel className="text-left">Name</FormLabel>
+                        <FormControl>
+                          <Input
+                            placeholder="Your name"
+                            className="text-black"
+                            {...field}
+                          />
+                        </FormControl>
+                        <FormMessage />
+                      </FormItem>
+                    )}
+                  />
+                  <FormField
+                    control={form.control}
+                    name="email"
+                    render={({ field }) => (
+                      <FormItem className="flex flex-col justify-left">
+                        <FormLabel className="text-left">Email</FormLabel>
+                        <FormControl>
+                          <Input
+                            placeholder="Your email"
+                            className="text-black"
+                            {...field}
+                          />
+                        </FormControl>
+                        <FormMessage />
+                      </FormItem>
+                    )}
+                  />
+                  <FormField
+                    control={form.control}
+                    name="message"
+                    render={({ field }) => (
+                      <FormItem className="flex flex-col justify-left">
+                        <FormLabel className="text-left">Message</FormLabel>
+                        <FormControl>
+                          <Textarea
+                            placeholder="Your message"
+                            className="max-h-96 text-black"
+                            {...field}
+                          />
+                        </FormControl>
+                        <FormMessage />
+                      </FormItem>
+                    )}
+                  />
+                  <Turnstile
+                    ref={turnstileRef}
+                    siteKey="0x4AAAAAAAfgP80mkF0iiKza"
+                  />
+                  <Button type="submit" disabled={isSubmitting}>
+                    {isSubmitting ? "Submitting..." : "Submit"}
+                  </Button>
+                  {isError && (
+                    <p className="text-red-500 mt-2">
+                      An error occurred. Please try again.
+                    </p>
+                  )}
+                </form>
+              </Form>
+            )}
+          </CardContent>
+        </Card>
+      </section>
+    </main>
+  );
+}
---
+++ pages/api/contact/create.js
@@ -0,0 +1,65 @@
+// pages/api/contact/create.js
+
+import nodeMailer from "nodemailer";
+
+export default async function handler(req, res) {
+    if (req.method !== "POST") {
+        return res.status(405).json({ error: "Method not allowed" });
+    }
+
+    const { name, email, message, cloudflareToken } = req.body;
+
+    if (!name || !email || !message) {
+        return res.status(400).json({ error: "All fields are required" });
+    }
+
+    if (cloudflareToken) {
+        try {
+            const response = await fetch(`/api/auth/turnstile-verify`, {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify({ cloudflareToken }),
+            });
+            const data = await response.json();
+            if (!data.success) {
+                return res.status(401).json({ error: "Invalid captcha" });
+            }
+        } catch (error) {
+            console.error(error);
+            return res.status(500).json({ error: "Failed to verify captcha" });
+        }
+    } else {
+        return res.status(401).json({ error: "Invalid captcha" });
+    }
+
+    try {
+        const transporter = nodeMailer.createTransport({
+            host: "smtp.gmail.com",
+            port: 465,
+            secure: true,
+            auth: {
+                user: "bestcodes.official@gmail.com",
+                pass: process.env.GMAIL_PSW,
+            },
+        });
+
+        const mailOptions = {
+            from: `BestCodes Website <bestcodes.official@gmail.com>`,
+            to: "bestcodes.official@gmail.com",
+            subject: `New Submission of BestCodes Contact Form - From ${name}`,
+            text: `Name: ${name}\nEmail: ${email}\nMessage: ${message}`,
+            html: `<p>Name: ${name}</p><p>Email: ${email}</p><p>Message: ${message}</p>`,
+        };
+
+        await transporter.sendMail(mailOptions);
+
+        return res.status(200).json({ success: true });
+    } catch (error) {
+        console.error(error);
+        return res.status(500).json({ error: "Failed to send email" });
+    }
+
+    return res.status(500).json({ error: "Internal server error" });
+}
