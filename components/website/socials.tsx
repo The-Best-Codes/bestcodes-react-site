706d816ba0a9f1be47e2f38e59aa267588d3ddc8
The-Best-Codes <best-codes@email.com>
2024-07-20 20:05:14 -0500

    update socials.tsx

--- components/website/socials.tsx
+++ components/website/socials.tsx
@@ -1,98 +1,68 @@
-import React, { useState } from "react";
-import { motion } from "framer-motion";
+import React from "react";
 import Link from "next/link";
 import Image from "next/image";
-import {
-  Card,
-  CardHeader,
-  CardFooter,
-  CardTitle,
-  CardDescription,
-  CardContent,
-} from "@/components/ui/card";
+import { Card, CardContent } from "@/components/ui/card";
+
+const socialLinks = [
+  {
+    href: "https://github.com/The-Best-Codes",
+    icon: "github.svg",
+    username: "@The-Best-Codes",
+  },
+  {
+    href: "https://dev.to/best_codes",
+    icon: "devdotto.svg",
+    username: "@best_codes",
+  },
+  {
+    href: "https://stackoverflow.com/users/20392792/best-codes",
+    icon: "stackoverflow.svg",
+    username: "@best-codes",
+  },
+  {
+    href: "https://codeium.com/profile/best_codes",
+    icon: "codeium.svg",
+    username: "@best_codes",
+  },
+  {
+    href: "https://discord.gg/HCZJVrrR",
+    icon: "discord.svg",
+    username: "@best_codes",
+  },
+  {
+    href: "https://www.printables.com/@Best_codes",
+    icon: "printables.svg",
+    username: "@Best_codes",
+  },
+  {
+    href: "https://x.com/the_best_codes",
+    icon: "x.svg",
+    username: "@the_best_codes",
+  },
+  { href: "mailto:best-codes@proton.me", icon: "email.svg", username: "Email" },
+];
 
 export default function SocialMediaLinks() {
   return (
-    <Card className="w-1/2">
-      <CardContent className="p-4 flex flex-col items-center justify-center gap-4">
-        <div className="flex flex-row w-full items-center justify-evenly gap-4">
-          <Link href="https://github.com/The-Best-Codes" target="_blank">
-            <div className="flex flex-col items-center justify-center gap-2">
-              <Image
-                src="/image/icons/github.svg"
-                alt="github"
-                width={400}
-                height={400}
-                className="h-36 w-36"
-              />
-              <span className="text-md text-blue-500">@The-Best-Codes</span>
-            </div>
-          </Link>
-          <Link href="https://dev.to/best_codes" target="_blank">
-            <div className="flex flex-col items-center justify-center gap-2">
-              <Image
-                src="/image/icons/devdotto.svg"
-                alt="devdotto"
-                width={400}
-                height={400}
-                className="h-36 w-36"
-              />
-              <span className="text-md text-blue-500">@best_codes</span>
-            </div>
-          </Link>
-          <Link
-            href="https://stackoverflow.com/users/20392792/best-codes"
-            target="_blank"
-          >
-            <div className="flex flex-col items-center justify-center gap-2">
-              <Image
-                src="/image/icons/stackoverflow.svg"
-                alt="stackoverflow"
-                width={400}
-                height={400}
-                className="h-36 w-36"
-              />
-              <span className="text-md text-blue-500">@best-codes</span>
-            </div>
-          </Link>
-        </div>
-        <div className="flex flex-row w-full items-center justify-evenly gap-4">
-          <Link href="https://codeium.com/profile/best_codes" target="_blank">
-            <div className="flex flex-col items-center justify-center gap-2">
-              <Image
-                src="/image/icons/codeium.svg"
-                alt="codeium"
-                width={400}
-                height={400}
-                className="h-36 w-36"
-              />
-              <span className="text-md text-blue-500">@best_codes</span>
-            </div>
-          </Link>
-          <Link href="https://discord.gg/HCZJVrrR" target="_blank">
-            <div className="flex flex-col items-center justify-center gap-2">
-              <Image
-                src="/image/icons/discord.svg"
-                alt="discord"
-                width={400}
-                height={400}
-                className="h-36 w-36"
-              />
-              <span className="text-md text-blue-500">@best_codes</span>
-            </div>
-          </Link>
-          <Link href="https://www.printables.com/@Best_codes" target="_blank">
-            <div className="flex flex-col items-center justify-center gap-2">
-              <Image
-                src="/image/icons/printables.svg"
-                alt="printables"
-                width={400}
-                height={400}
-                className="h-36 w-36"
-              />
-              <span className="text-md text-blue-500">@Best_codes</span>
-            </div>
-          </Link>
+    <Card className="w-full max-w-4xl mx-auto">
+      <CardContent className="p-4">
+        <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-4 gap-4">
+          {socialLinks.map((link, index) => (
+            <Link key={index} href={link.href} target="_blank">
+              <div className="flex flex-col items-center justify-center gap-2">
+                <Image
+                  src={`/image/icons/${link.icon}`}
+                  alt={link.icon.split(".")[0]}
+                  width={400}
+                  height={400}
+                  className="h-20 w-20 sm:h-24 sm:w-24 md:h-28 md:w-28 lg:h-32 lg:w-32"
+                />
+                <span className="text-sm sm:text-md text-blue-500 text-center">
+                  {link.username}
+                </span>
+              </div>
+            </Link>
+          ))}
         </div>
       </CardContent>
     </Card>
