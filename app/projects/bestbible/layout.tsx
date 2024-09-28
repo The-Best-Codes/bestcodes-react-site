24ae2308cc676871c64b9d3146d52b2f4d57e6cc
The-Best-Codes <bestcodes.official@gmail.com>
2024-09-27 21:45:34 -0500

    Update SEO

--- app/projects/bestbible/layout.tsx
+++ app/projects/bestbible/layout.tsx
@@ -4,9 +4,10 @@ import { Inter } from "next/font/google";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "BestBible - The Best Bible API",
+  title:
+    "BestBible - Fetch, parse, and analyze the Bible easily with JavaScript | BestCodes",
   description:
-    "The BestBible API is a free, open-source API for accessing Bible data. It has a simple and intuitive API, and is easily extensible.",
+    "The BestBible npm library is a free, open-source library for accessing Bible data. It has a simple and intuitive API, and is easily extensible.",
 };
 
 export default function RootLayout({
