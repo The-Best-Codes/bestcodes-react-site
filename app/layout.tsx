91be604d75d33df1c5df549604219669caaa9dd3
The-Best-Codes <bestcodes.official@gmail.com>
2024-10-02 16:43:58 -0500

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,7 +7,24 @@ const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "BestCodes' Website - Christian, Coder, Creator | Home",
-  description: "BestCodes' Official Website",
+  description: "BestCodes' Official Website | Christian, Coder, Creator",
+  applicationName: "BestCodes' Website",
+  authors: [{ name: "BestCodes", url: "https://bestcodes.dev" }],
+  keywords: [
+    "BestCodes",
+    "The-Best-Codes",
+    "Christian",
+    "coder",
+    "creator",
+    "free",
+    "javascript",
+    "projects",
+    "codequill",
+    "best-bible",
+    "best-holiday",
+  ],
+  creator: "BestCodes",
+  publisher: "BestCodes",
 };
 
 export default function RootLayout({
