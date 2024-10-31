c4427d694e8a24a149795b52169ff27540383db0
The-Best-Codes <bestcodes.official@gmail.com>
2024-10-31 12:01:51 -0500

    Bulk fix all layout.tsx files to use local font

--- app/projects/bestbible/layout.tsx
+++ app/projects/bestbible/layout.tsx
@@ -1,8 +1,16 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import localFont from "next/font/local";
 
-const inter = Inter({ subsets: ["latin"] });
-
+const geistSans = localFont({
+  src: "../../fonts/GeistVF.woff",
+  variable: "--font-geist-sans",
+  weight: "100 900",
+});
+const geistMono = localFont({
+  src: "../../fonts/GeistMonoVF.woff",
+  variable: "--font-geist-mono",
+  weight: "100 900",
+});
 export const metadata: Metadata = {
   title:
     "BestBible - Fetch, parse, and analyze the Bible easily with JavaScript | BestCodes",
@@ -17,7 +25,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={`${geistSans.variable} ${geistMono.variable}`}>
+        {children}
+      </body>
     </html>
   );
 }
--- app/projects/qrcode/layout.tsx
+++ app/projects/qrcode/layout.tsx
@@ -1,8 +1,16 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import localFont from "next/font/local";
 
-const inter = Inter({ subsets: ["latin"] });
-
+const geistSans = localFont({
+  src: "../../fonts/GeistVF.woff",
+  variable: "--font-geist-sans",
+  weight: "100 900",
+});
+const geistMono = localFont({
+  src: "../../fonts/GeistMonoVF.woff",
+  variable: "--font-geist-mono",
+  weight: "100 900",
+});
 export const metadata: Metadata = {
   title: "Free, No Registration QR Code Generator | BestCodes",
   description:
@@ -16,7 +24,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={`${geistSans.variable} ${geistMono.variable}`}>
+        {children}
+      </body>
     </html>
   );
 }
