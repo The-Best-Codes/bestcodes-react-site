d980496ea4d8b64cd9c00fbbcb60c7e52cf2eb09
The-Best-Codes <bestcodes.official@gmail.com>
2024-11-24 12:28:51 -0600

    Skip linting and ts checks in prod

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    typescript: {
+        ignoreBuildErrors: true,
+    },
+    eslint: {
+        ignoreDuringBuilds: true,
+    },
     images: {
         "remotePatterns": [
             {
