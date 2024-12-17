f64cde08e491d0963b88496f80eae654c8290a11
The-Best-Codes <bestcodes.official@gmail.com>
2024-12-17 09:14:56 -0600

    Add sentry

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,68 +3,68 @@ import localFont from "next/font/local";
 import "./globals.css";
 
 const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
+	src: "./fonts/GeistVF.woff",
+	variable: "--font-geist-sans",
+	weight: "100 900",
 });
 const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
+	src: "./fonts/GeistMonoVF.woff",
+	variable: "--font-geist-mono",
+	weight: "100 900",
 });
 
 export const metadata: Metadata = {
-  title: "BestCodes' Website - Christian, Coder, Creator | Home",
+	title: "BestCodes' Website - Christian, Coder, Creator | Home",
 
-  description:
-    "BestCodes is a full-stack developer, christian and creator. This is BestCodes' Official Website.",
+	description:
+		"BestCodes is a full-stack developer, christian and creator. This is BestCodes' Official Website.",
 
-  applicationName: "BestCodes' Website",
-  authors: [{ name: "BestCodes", url: "https://bestcodes.dev" }],
+	applicationName: "BestCodes' Website",
+	authors: [{ name: "BestCodes", url: "https://bestcodes.dev" }],
 
-  keywords: [
-    "BestCodes",
-    "The-Best-Codes",
-    "Christian",
-    "coder",
-    "creator",
-    "free",
-    "javascript",
-    "projects",
-    "codequill",
-    "best-bible",
-    "best-holiday",
-    "math",
-  ],
+	keywords: [
+		"BestCodes",
+		"The-Best-Codes",
+		"Christian",
+		"coder",
+		"creator",
+		"free",
+		"javascript",
+		"projects",
+		"codequill",
+		"best-bible",
+		"best-holiday",
+		"math",
+	],
 
-  creator: "BestCodes",
-  publisher: "BestCodes",
+	creator: "BestCodes",
+	publisher: "BestCodes",
 
-  openGraph: {
-    title: "BestCodes' Website - Christian, Coder, Creator | Home",
-    description: "BestCodes' Official Website | Christian, Coder, Creator",
-    url: "https://bestcodes.dev",
-    siteName: "BestCodes' Website",
-    images: [
-      {
-        url: "https://bestcodes.dev/image/og-image.png",
-        width: 1200,
-        height: 630,
-      },
-    ],
-  },
+	openGraph: {
+		title: "BestCodes' Website - Christian, Coder, Creator | Home",
+		description: "BestCodes' Official Website | Christian, Coder, Creator",
+		url: "https://bestcodes.dev",
+		siteName: "BestCodes' Website",
+		images: [
+			{
+				url: "https://bestcodes.dev/image/og-image.png",
+				width: 1200,
+				height: 630,
+			},
+		],
+	},
 };
 
 export default function RootLayout({
-  children,
+	children,
 }: Readonly<{
-  children: React.ReactNode;
+	children: React.ReactNode;
 }>) {
-  return (
-    <html lang="en" suppressHydrationWarning>
-      <body className={`${geistSans.variable} ${geistMono.variable}`}>
-        {children}
-      </body>
-    </html>
-  );
+	return (
+		<html lang="en" suppressHydrationWarning>
+			<body className={`${geistSans.variable} ${geistMono.variable}`}>
+				{children}
+			</body>
+		</html>
+	);
 }
---
+++ sentry.server.config.ts
@@ -0,0 +1,15 @@
+// This file configures the initialization of Sentry on the server.
+// The config you add here will be used whenever the server handles a request.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://0439e24e7e24b2a39b0f36ee306b1eb9@o4508483919609856.ingest.us.sentry.io/4508483950411776",
+
+  // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
+  tracesSampleRate: 1,
+
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false,
+});
