79dd0dbf15fa8201691a5477c9401578d657ae2a
The-Best-Codes <bestcodes.official@gmail.com>
2024-12-17 12:33:55 -0600

    [BETA] Allow bug reports

--- sentry.client.config.ts
+++ sentry.client.config.ts
@@ -5,24 +5,27 @@
 import * as Sentry from "@sentry/nextjs";
 
 Sentry.init({
-  dsn: "https://0439e24e7e24b2a39b0f36ee306b1eb9@o4508483919609856.ingest.us.sentry.io/4508483950411776",
+	dsn: "https://0439e24e7e24b2a39b0f36ee306b1eb9@o4508483919609856.ingest.us.sentry.io/4508483950411776",
 
-  // Add optional integrations for additional features
-  integrations: [
-    Sentry.replayIntegration(),
-  ],
+	// Add optional integrations for additional features
+	integrations: [
+		Sentry.replayIntegration(),
+		Sentry.feedbackIntegration({
+			colorScheme: "system",
+		}),
+	],
 
-  // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
-  tracesSampleRate: 1,
+	// Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
+	tracesSampleRate: 1,
 
-  // Define how likely Replay events are sampled.
-  // This sets the sample rate to be 10%. You may want this to be 100% while
-  // in development and sample at a lower rate in production
-  replaysSessionSampleRate: 0.1,
+	// Define how likely Replay events are sampled.
+	// This sets the sample rate to be 10%. You may want this to be 100% while
+	// in development and sample at a lower rate in production
+	replaysSessionSampleRate: 0.1,
 
-  // Define how likely Replay events are sampled when an error occurs.
-  replaysOnErrorSampleRate: 1.0,
+	// Define how likely Replay events are sampled when an error occurs.
+	replaysOnErrorSampleRate: 1.0,
 
-  // Setting this option to true will print useful information to the console while you're setting up Sentry.
-  debug: false,
+	// Setting this option to true will print useful information to the console while you're setting up Sentry.
+	debug: false,
 });
