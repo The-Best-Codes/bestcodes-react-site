e3951224ed93deadf6db53fdaaa6a3feb49f38fc
The-Best-Codes <bestcodes.official@gmail.com>
2024-12-20 19:18:39 -0600

    feat: add back user feeback from sentry.io

--- sentry.client.config.ts
+++ sentry.client.config.ts
@@ -10,6 +10,9 @@ Sentry.init({
   // Add optional integrations for additional features
   integrations: [
     Sentry.replayIntegration(),
+    Sentry.feedbackIntegration({
+      colorScheme: "system",
+    }),
   ],
 
   // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
