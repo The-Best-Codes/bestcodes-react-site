a54f461128c011edef7a186f4e9fdbdbbaacb761
The-Best-Codes <bestcodes.official@gmail.com>
2024-12-31 09:01:59 -0600

    bunilla metadata update

--- app/bunilla/layout.tsx
+++ app/bunilla/layout.tsx
@@ -11,10 +11,38 @@ const geistMono = localFont({
   variable: "--font-geist-mono",
   weight: "100 900",
 });
+
 export const metadata: Metadata = {
   title: "Bunilla - Production builds in milliseconds",
 
   description: "Bunilla builds Next.js apps in less than a second.",
+
+  applicationName: "Bunilla",
+  authors: [{ name: "BestCodes", url: "https://bestcodes.dev" }],
+
+  keywords: [
+    "framework",
+    "nextjs",
+    "bunilla",
+    "bun",
+    "build",
+    "production",
+    "buildtime",
+    "builds",
+    "faster",
+    "faster-than-before",
+  ],
+
+  creator: "BestCodes",
+  publisher: "BestCodes",
+
+  openGraph: {
+    title: "Bunilla - Production builds in milliseconds",
+    description: "Bunilla builds Next.js apps in less than a second.",
+    url: "https://bestcodes.dev/bunilla?ref=og",
+    siteName: "Bunilla",
+    images: [],
+  },
 };
 
 export default function RootLayout({
