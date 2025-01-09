234f06c2e10e42394280faf7a04b4730764ed688
The-Best-Codes <bestcodes.official@gmail.com>
2025-01-08 19:59:42 -0600

    update deps

--- components/pages/bunilla/terminal/Terminal.tsx
+++ components/pages/bunilla/terminal/Terminal.tsx
@@ -1,7 +1,7 @@
 "use client";
 
-import { motion } from "framer-motion";
 import { Terminal as TerminalIcon } from "lucide-react";
+import { motion } from "motion/react";
 import { TerminalOutput } from "./TerminalOutput";
 
 interface TerminalProps {
